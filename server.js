72f459cd63457cd52d31a4dd408c08093ede7401
R3zu4C <a.patanwal@protonmail.com>
2022-06-01 21:23:57 +0530

    many-to-many SFU server side implemented

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 import express from 'express';
-import fs from 'fs';
 import webrtc from 'wrtc';
-import bodyParser from 'body-parser';
+import WebSocket from 'ws';
+import { uuidv4 } from 'uuid';
+
+const WebSocketServer = WebSocket.Server;
 
 const app = express();
 
-let senderStream;
-
 app.use(express.static('public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/broadcast', (req, res) => {
-  res.end(fs.readFileSync('./views/broadcast.html'));
-})
-
-app.get('/viewer', (req, res) => {
-  res.end(fs.readFileSync('./views/viewer.html'));
-})
 
 const iceConfig = {
   iceServers: [
@@ -33,39 +23,140 @@ const iceConfig = {
   ]
 }
 
-app.post('/broadcast', async (req, res) => {
-  const sdp = req.body.sdp;
-  const peerConnection = new webrtc.RTCPeerConnection(iceConfig);
+const webServer = app.listen(5000, () => console.log('Server running on port 5000...'));
 
-  
-  peerConnection.ontrack = (event) => {
-    senderStream = event.streams[0];
-  }
+const peers = new Map();
+const consumers = new Map();
 
-  const desc = new webrtc.RTCSessionDescription(sdp);
-  await peerConnection.setRemoteDescription(desc);
+const webSocketServer = new WebSocketServer({ server: webServer });
 
-  const answer = await peerConnection.createAnswer();
-  await peerConnection.setLocalDescription(answer);
+webSocketServer.broadcast = (message) => {
+  peers.forEach((peer) => {
+      if (peer.socket.readyState === WebSocket.OPEN) {
+          peer.socket.send(message);
+      }
+  });
+};
 
-  res.json({ sdp: peerConnection.localDescription });
+webSocketServer.on("connection", async (socket) => {
+  const peerId = uuidv4();
+  socket.id = peerId;
 
+  socket.on('close', (e) => {
+    peers.delete(socket.id);
+    consumers.delete(socket.id);
+
+    webSocketServer.broadcast(JSON.stringify({
+      type: 'user_left',
+      id: socket.id,
+    }))
+  })
+
+  webSocketServer.send(JSON.stringify({
+    type: 'welcome',
+    id: socket.id,
+  }))
+
+  socket.on('message', async (message) => {
+    const msg = JSON.parse(message);
+
+    if(msg.type === 'connect') {
+      const peer = new webrtc.RTCPeerConnection(iceConfig);
+
+      peers.set(socket.id, { socket: socket });
+      peers.get(socket.id).username = msg.username;
+      peers.get(socket.id).peer = peer;
+
+      peer.ontrack((event) => {
+        if(event.streams[0]) {
+          peers.get(socket.id).stream = event.streams[0];
+
+          const payload = {
+            type: 'new_producer',
+            id: socket.id,
+            username: peers.get(socket.id).username,
+          }
+
+          webSocketServer.broadcast(JSON.stringify(payload));
+        }
+      })
+
+      const desc = webrtc.RTCSessionDescription(msg.sdp);
+      await peer.setRemoteDescription(desc);
+      const answer = await peer.createAnswer();
+      await peer.setLocalDescription(answer);
+
+      const payload = {
+        type: 'answer',
+        sdp: peer.localDescription,
+      }
+
+      socket.send(JSON.stringify(payload));
+
+    } 
+    else if(msg.type === 'get_peers') {
+      const peerList = [];
+      peers.forEach((peer, key) => {
+        peerList.push({
+          id: key,
+          username: peer.username,
+        })
+      })
+
+      const payload = {
+        type: 'peers',
+        peers: list,
+      }
+
+      socket.send(JSON.stringify(payload));
+
+    } 
+    else if(msg.type === 'consume') {
+      const remoteUser = peers.get(msg.producerId);
+      
+      const peer = new webrtc.RTCPeerConnection(iceConfig);
+      consumers.set(msg.consumerId, peer);
+
+      remoteUser.stream.getTracks().forEach((track) => peer.addTrack((track, remoteUser.stream)))
+
+      const desc = webrtc.RTCSessionDescription(msg.sdp);
+      await peer.setRemoteDescription(desc);
+      const answer = await peer.createAnswer();
+      await peer.setLocalDescription(answer);
+
+      const payload = {
+        type: 'consume',
+        sdp: peer.localDescription,
+        consumerId: msg.consumerId,
+        producerId: msg.producerId,
+        username: remoteUser.username,
+      }
+
+      socket.send(JSON.stringify(payload));
+
+    } 
+    else if(msg.type === 'ice') {
+      const user = peers.get(msg.id);
+
+      if(user.peer) {
+        const iceCandidate = new webrtc.RTCIceCandidate(msg.ice);
+        await user.peer.addIceCandidate(iceCandidate);
+      }
+
+    } 
+    else if(msg.type === 'cosumer_ice') {
+      const user = consumers.get(msg.id);
+
+      if(user.peer) {
+        const iceCandidate = new webrtc.RTCIceCandidate(msg.ice);
+        await user.peer.addIceCandidate(iceCandidate);
+      }
+
+    }
+    else webSocketServer.broadcast(msg);
+  })
+
+  socket.on('error', (error) => socket.terminate());
 })
 
-app.post('/viewer', async (req, res) => {
-  const sdp = req.body.sdp;
-  const peerConnection = new webrtc.RTCPeerConnection(iceConfig);
 
-  const desc = new webrtc.RTCSessionDescription(sdp);
-  await peerConnection.setRemoteDescription(desc);
-
-  senderStream.getTracks().forEach((track) => peerConnection.addTrack(track, senderStream));
-
-  const answer = await peerConnection.createAnswer();
-  await peerConnection.setLocalDescription(answer);
-
-  res.json({ sdp: peerConnection.localDescription });
-
-})
-
-app.listen(5000, () => console.log("Server running on port 5000..."));
